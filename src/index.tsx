5583ad51aacb41ed5d8552c4ce853f07a0d90384
Unai Bolivar <ujbolivar@gmail.com>
2020-01-10 15:54:50 +0100

    Second commit: Can add chores to list, displays chores, complete button toggle functionality working.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,56 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
 export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
   return (
     <Fragment>
-        <h1>To Do List</h1>
-      <form>
-        <input type="text" required />
+      <h1>To Do List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Submit</button>
       </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => (
+          <Fragment>
+            <div key={index}>{todo.text}</div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {todo.complete ? "Complete" : "Incomplete"}
+            </button>
+          </Fragment>
+        ))}
+      </section>
     </Fragment>
   );
 }
